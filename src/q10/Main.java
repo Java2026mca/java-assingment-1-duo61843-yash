6400308149c193fd0e6d7557e92c5b968891696e
duo61843-yash <duo61843@gmail.com>
2026-04-07 16:34:33 +0530

    Update Main.java

--- src/q10/Main.java
+++ src/q10/Main.java
@@ -4,18 +4,31 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // TODO: Evaluate a postfix (Reverse Polish Notation) expression
-        //       Operands are non-negative integers, operators are: + - * /
-        //       Use a stack to evaluate
-        //       Input: single line, tokens separated by spaces
-        //       Output: integer result
-        //
-        // Input: 5 1 2 + 4 * + 3 -
-        // Output: 14
-        //
-        // Explanation: 5 + ((1+2)*4) - 3 = 5 + 12 - 3 = 14
+        // Read the whole line as postfix expression
+        String line = sc.nextLine().trim();
+        String[] tokens = line.split("\\s+");
 
-        String line = sc.nextLine();
+        Stack<Integer> stack = new Stack<>();
 
+        for (String token : tokens) {
+            if (token.matches("-?\\d+")) {
+                // number
+                stack.push(Integer.parseInt(token));
+            } else {
+                // operator
+                int b = stack.pop();
+                int a = stack.pop();
+
+                switch (token) {
+                    case "+": stack.push(a + b); break;
+                    case "-": stack.push(a - b); break;
+                    case "*": stack.push(a * b); break;
+                    case "/": stack.push(a / b); break;
+                }
+            }
+        }
+
+        // Final answer
+        System.out.print(stack.pop());
     }
 }
