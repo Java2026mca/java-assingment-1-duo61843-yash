72faf2718452a397e313384c41d068030e6a3126
duo61843-yash <duo61843@gmail.com>
2026-04-07 16:01:36 +0530

    Update Main.java

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Read input
         int n = sc.nextInt();
         int[] arr = new int[n];
 
@@ -14,7 +13,7 @@ public class Main {
 
         int swaps = 0;
 
-        // Count inversions (minimum adjacent swaps needed)
+        // Count inversions
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 if (arr[i] > arr[j]) {
@@ -23,21 +22,16 @@ public class Main {
             }
         }
 
-        // Sort the array
+        // Sort array
         Arrays.sort(arr);
 
         // Print sorted array
         for (int i = 0; i < n; i++) {
+            if (i > 0) System.out.print(" ");
             System.out.print(arr[i]);
-            if (i < n - 1) {
-                System.out.print(" ");
-            }
         }
 
-        // Print swaps
         System.out.println();
         System.out.println("Swaps: " + swaps);
-
-        sc.close();
     }
 }
