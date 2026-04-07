628b78ecc53a60f9bb5ed3e16b6b11a426bf0669
duo61843-yash <duo61843@gmail.com>
2026-04-07 16:18:20 +0530

    Update Main.java

--- src/q09/Main.java
+++ src/q09/Main.java
@@ -1,49 +1,28 @@
 import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int r1 = sc.nextInt();
-        int c1 = sc.nextInt();
-        int[][] mat1 = new int[r1][c1];
-        for (int i = 0; i < r1; i++) {
-            for (int j = 0; j < c1; j++) {
-                mat1[i][j] = sc.nextInt();
-            }
-        }
-
-        int r2 = sc.nextInt();
-        int c2 = sc.nextInt();
-        int[][] mat2 = new int[r2][c2];
-        for (int i = 0; i < r2; i++) {
-            for (int j = 0; j < c2; j++) {
-                mat2[i][j] = sc.nextInt();
-            }
-        }
-
-        if (c1 != r2) {
-            System.out.println("Cannot multiply matrices: incompatible dimensions");
-            return;
-        }
-
-        int[][] result = new int[r1][c2];
-        for (int i = 0; i < r1; i++) {
-            for (int j = 0; j < c2; j++) {
-                for (int k = 0; k < c1; k++) {
-                    result[i][j] += mat1[i][k] * mat2[k][j];
-                }
-            }
-        }
-
-        for (int i = 0; i < r1; i++) {
-            for (int j = 0; j < c2; j++) {
-                System.out.print(result[i][j]);
-                if (j < c2 - 1) System.out.print(" ");
+        int n1 = sc.nextInt(), m1 = sc.nextInt();
+        int[][] a = new int[n1][m1];
+        for (int i = 0; i < n1; i++)
+            for (int j = 0; j < m1; j++)
+                a[i][j] = sc.nextInt();
+        int n2 = sc.nextInt(), m2 = sc.nextInt();
+        int[][] b = new int[n2][m2];
+        for (int i = 0; i < n2; i++)
+            for (int j = 0; j < m2; j++)
+                b[i][j] = sc.nextInt();
+        int[][] c = new int[n1][m2];
+        for (int i = 0; i < n1; i++)
+            for (int j = 0; j < m2; j++)
+                for (int k = 0; k < m1; k++)
+                    c[i][j] += a[i][k] * b[k][j];
+        for (int i = 0; i < n1; i++){
+            for (int j = 0; j < m2; j++){
+                System.out.print(c[i][j]);
+                if (j < m2 - 1) System.out.print(" ");
             }
             System.out.println();
         }
-
-        sc.close();
     }
 }
