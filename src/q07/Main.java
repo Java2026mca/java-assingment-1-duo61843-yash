e143f1a69e6ecfb152d06dc9d436bbc1a93f857b
duo61843-yash <duo61843@gmail.com>
2026-04-07 16:51:50 +0530

    Update Main.java

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -13,9 +13,9 @@ public class Main {
 
         int swaps = 0;
 
-       
+        
         for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
+            for (int j = 0; j < n - 1; j++) {   
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
