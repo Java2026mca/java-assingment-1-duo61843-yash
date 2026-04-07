a39bfe38ffcad63e436f4c2f2ee7760418e681eb
duo61843-yash <duo61843@gmail.com>
2026-04-07 16:14:43 +0530

    Update Main.java

--- src/q09/Main.java
+++ src/q09/Main.java
@@ -3,22 +3,47 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
-        // TODO: Read two n×n matrices A and B, multiply them → print result C = A×B
-        //       Input: matrix A row by row, then matrix B row by row
-        //       Output: result matrix row by row, values separated by spaces
-        //
-        // Input:
-        // 2
-        // 1 2
-        // 3 4
-        // 5 6
-        // 7 8
-        //
-        // Output:
-        // 19 22
-        // 43 50
+        int r1 = sc.nextInt();
+        int c1 = sc.nextInt();
+        int[][] mat1 = new int[r1][c1];
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c1; j++) {
+                mat1[i][j] = sc.nextInt();
+            }
+        }
 
+        int r2 = sc.nextInt();
+        int c2 = sc.nextInt();
+        int[][] mat2 = new int[r2][c2];
+        for (int i = 0; i < r2; i++) {
+            for (int j = 0; j < c2; j++) {
+                mat2[i][j] = sc.nextInt();
+            }
+        }
+
+        if (c1 != r2) {
+            System.out.println("Cannot multiply matrices: incompatible dimensions");
+            return;
+        }
+
+        int[][] result = new int[r1][c2];
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c2; j++) {
+                for (int k = 0; k < c1; k++) {
+                    result[i][j] += mat1[i][k] * mat2[k][j];
+                }
+            }
+        }
+
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c2; j++) {
+                System.out.print(result[i][j]);
+                if (j < c2 - 1) System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
     }
 }
