439eb40dd107e7ce4132d24d8c1390c98df45150
duo61843-yash <duo61843@gmail.com>
2026-04-07 15:48:39 +0530

    Update Main.java

--- src/q06/Main.java
+++ src/q06/Main.java
@@ -8,7 +8,10 @@ public class Main {
         int a = 0, b = 1;
 
         for (int i = 0; i < n; i++) {
-            System.out.print(a + " ");
+            System.out.print(a);
+            if (i != n - 1) {
+                System.out.print(" ");
+            }
             int next = a + b;
             a = b;
             b = next;
