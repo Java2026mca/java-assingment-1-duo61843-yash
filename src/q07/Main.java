b7633fbc0053da63019eb6059aa617c8c22accd5
duo61843-yash <duo61843@gmail.com>
2026-04-07 16:49:31 +0530

    Update Main.java

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -1,1 +1,37 @@
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = Integer.parseInt(sc.nextLine().trim());
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int swaps = 0;
+
+       
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swaps++;
+                }
+            }
+        }
+
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]);
+            if (i < n - 1) System.out.print(" ");
+        }
+
+        System.out.println();
+        System.out.println("Swaps: " + swaps);
+    }
+}
