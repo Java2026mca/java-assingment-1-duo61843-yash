5febebea20013743430f49eb0102ce7908bd523d
duo61843-yash <duo61843@gmail.com>
2026-04-07 11:07:37 +0530

    Update Main.java

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -5,16 +5,20 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // TODO: Fill an N×N matrix in clockwise spiral order starting from 1
-        //       Print each row with values separated by single space
-        //       Then print: "Diagonal: X" where X = sum of primary diagonal (top-left to bottom-right)
-        //
-        // Input: 3
-        // Output:
-        // 1 2 3
-        // 8 9 4
-        // 7 6 5
-        // Diagonal: 15
+        for (int i = 0; i < n; i++) {
+            int val = 1;
 
+            for (int j = 0; j <= i; j++) {
+                System.out.print(val);
+
+                if (j < i) {
+                    System.out.print(" ");
+                }
+
+                val = val * (i - j) / (j + 1);
+            }
+
+            System.out.println();
+        }
     }
 }
