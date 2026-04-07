34aa44e889df8e9f9081ebf48462b71bb23472f3
duo61843-yash <duo61843@gmail.com>
2026-04-07 11:52:01 +0530

    Update Main.java

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -4,60 +4,41 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
         int[][] mat = new int[n][n];
 
+        int num = 1;
         int top = 0, bottom = n - 1;
         int left = 0, right = n - 1;
-        int num = 1;
 
-      
-        while (top <= bottom && left <= right) {
-
+        while (num <= n * n) {
             
-            for (int j = left; j <= right; j++) {
-                mat[top][j] = num++;
-            }
+            for (int i = left; i <= right && num <= n * n; i++) mat[top][i] = num++;
             top++;
-
-         
-            for (int i = top; i <= bottom; i++) {
-                mat[i][right] = num++;
-            }
+        
+            for (int i = top; i <= bottom && num <= n * n; i++) mat[i][right] = num++;
             right--;
-
-          
-            if (top <= bottom) {
-                for (int j = right; j >= left; j--) {
-                    mat[bottom][j] = num++;
-                }
-                bottom--;
-            }
-
-         
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    mat[i][left] = num++;
-                }
-                left++;
-            }
+           
+            for (int i = right; i >= left && num <= n * n; i--) mat[bottom][i] = num++;
+            bottom--;
+           
+            for (int i = bottom; i >= top && num <= n * n; i--) mat[i][left] = num++;
+            left++;
         }
 
-        
+      
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(mat[i][j]);
-                if (j < n - 1) System.out.print(" ");
+                if (j != n - 1) System.out.print(" ");
             }
             System.out.println();
         }
 
-       
+        
         int diagSum = 0;
-        for (int i = 0; i < n; i++) {
-            diagSum += mat[i][i];
-        }
-
+        for (int i = 0; i < n; i++) diagSum += mat[i][i];
         System.out.println("Diagonal: " + diagSum);
+
+        sc.close();
     }
 }
